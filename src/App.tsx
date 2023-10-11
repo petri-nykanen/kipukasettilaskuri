dcc178d7b1886fdd66d685405d4f3200b0ec1781
petri-nykanen <petri.nykaenen@gmail.com>
2023-10-11 15:54:49 +0300

    Init vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,142 +1,35 @@
-import { Button, Container, Typography } from "@mui/material";
-import React, { useContext } from "react";
-import { Bolus } from "./components/Bolustaulukko.tsx/Bolus";
-import { BolusYhteensa } from "./components/Bolustaulukko.tsx/BolusYhteensa";
-import { Footer } from "./components/Footer";
-import { Valinta } from "./components/Valinta";
-import { Yhteensa } from "./components/Valmistetaulukko/Yhteensa";
-import { Context } from "./context/context";
-import TaulukkoContainer from "./components/Valmistetaulukko/Taulukko-container";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const { ohje, setOhje, laakeTaulukko } = useContext(Context);
-  const renderInstructions = () => {
-    switch (ohje.sivu) {
-      case 1:
-        return (
-          <>
-            <Typography sx={{ padding: "20px" }}>
-              Määrää lääkkeen vrk-annos mg (oranssi kenttä), saat vastaavan määrä ml:ssa viereiseen
-              sarakkeeseen
-            </Typography>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setOhje({ ...ohje, sivu: 2 });
-              }}
-            >
-              Seuraava
-            </Button>
-          </>
-        );
-      case 2:
-        return (
-          <>
-            <Typography sx={{ padding: "20px" }}>
-              Katso liuoksen minimimäärä vuorokaudessa ja tunnissa.
-            </Typography>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setOhje({ ...ohje, sivu: 3 });
-              }}
-            >
-              Seuraava
-            </Button>
-          </>
-        );
-      case 3:
-        return (
-          <>
-            <Typography sx={{ padding: "20px" }}>
-              Määrää tarvittaessa NaCl tilavuus (oranssi kenttä) pyrkien pieneen infuusionopeuteen
-              (0,1-2,0 ml/h), tarkista samalla lääkeannostelijan riittävyys vuorokausissa (vrk) ja
-              tuntia (h)
-            </Typography>
-            <Button
-              sx={{ display: "block" }}
-              variant="outlined"
-              onClick={() => {
-                setOhje({ ...ohje, sivu: 4 });
-              }}
-            >
-              Seuraava
-            </Button>
-          </>
-        );
-      case 4:
-        return (
-          <>
-            <Typography sx={{ padding: "20px" }}>
-              Tarvittaessa, määritä bolukset alas taulukkoon ml ja max bol/h
-            </Typography>
-            <Button
-              sx={{ marginLeft: "10px" }}
-              variant="outlined"
-              onClick={() => {
-                setOhje({ ...ohje, sivu: 5 });
-              }}
-            >
-              Seuraava
-            </Button>
-          </>
-        );
-      case 5:
-        return (
-          <>
-            <Typography sx={{ padding: "20px" }}>
-              Voit muuttaa lääkeaineen vahvuutta tuplaklikkaamalla kenttää, jolloin avautuu
-              tekstikenttä muokkaamista varten.
-            </Typography>
-            <Button
-              sx={{ display: "block" }}
-              variant="outlined"
-              onClick={() => {
-                setOhje({ ...ohje, auki: false, sivu: 0 });
-              }}
-            >
-              Seuraava
-            </Button>
-          </>
-        );
-    }
-  };
+  const [count, setCount] = useState(0)
 
   return (
-    <Container sx={{ width: "1200px", display: "flex", flexWrap: "wrap" }}>
-      {renderInstructions()}
-      <br />
-      {ohje.auki === false && laakeTaulukko.length > 0 ? (
-        <Button
-          variant="outlined"
-          sx={{ margin: "10px", display: "block" }}
-          onClick={() => setOhje({ ...ohje, auki: true, sivu: 1 })}
-        >
-          OHJE
-        </Button>
-      ) : null}
-
-      {laakeTaulukko.length > 0 ? (
-        <Container sx={{ display: "flex", flexDirection: "column" }}>
-          <Container sx={{ display: "flex", flexDirection: "row" }}>
-            <TaulukkoContainer />
-            <Yhteensa />
-          </Container>
-
-          <Container sx={{ display: "flex", flexDirection: "row", mt: "1%" }}>
-            <Bolus />
-            <BolusYhteensa />
-          </Container>
-        </Container>
-      ) : (
-        <>
-          <Typography sx={{ padding: "20px" }}>Aloita lisäämällä taulukkoon lääkeaine</Typography>
-          <Valinta />
-        </>
-      )}
-      <Footer />
-    </Container>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
