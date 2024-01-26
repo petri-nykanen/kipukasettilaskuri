3d955b5c19a155c6ebf4dba62a226852c87502af
cpeny003-xamk <cpeny003@edu.xamk.fi>
2024-01-26 04:31:27 +0200

    Updates

---
+++ precache-manifest.8dbda7aff0f60b71fd0822c4f03c6cff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c12d49205e284d1b30309af73a93be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88524e58e837246c8d31",
+    "url": "/static/js/2.21980f07.chunk.js"
+  },
+  {
+    "revision": "f4d02aacc69b3b2c9763",
+    "url": "/static/js/main.5de10d40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
